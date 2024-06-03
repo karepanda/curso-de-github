8475a24a6c4dfcf0e8174a7759c79e85645fce83
kare <karelysperezs@gmail.com>
2024-06-03 17:38:57 +0200

    Decimales al total del carrito

--- git.js
+++ git.js
@@ -1,5 +1,5 @@
 const drawTotalCart = (cart) => {
     const DOMCartTotal = document.getElementById("cart-total");
-    DOMCartTotal.innerHTML = '$${cart.total}'
+    DOMCartTotal.innerHTML = '$${cart.total.toFixed(2)}';
 };
 console.log("hola bitch")
